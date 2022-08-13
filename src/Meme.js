0a7e87215e54b80c84b6e30a351115a79d7656c8
Tea Aleksov <aleksov.tea@gmail.com>
2022-08-13 20:06:12 +0200

    comm

--- src/Meme.js
+++ src/Meme.js
@@ -1,21 +1,29 @@
 import React from "react"
 import memesData from "./memesData"
 export default function Meme(){
-    
+
+    const [memeImage , setMemeImage] = React.useState("")
+
     function getMemeImage(){
         const imgArr = memesData.data.memes
         const random = Math.floor(Math.random() * imgArr.length)
-        const memeImage = imgArr[random].url
+       setMemeImage(imgArr[random].url)
         console.log(memeImage)
     }
 
+    
     return(
-        <div className="input-container">
+        <div>
+            <div className="input-container">
             <div className="input">
                 <input className="up" type="text" placeholder="Top text"></input>
                 <input className="down" type="text" placeholder="Bottom text"></input>
             </div>
             <button onClick={getMemeImage}>Get a new meme image</button>
+
         </div>
+        <img src={memeImage} className="meme-img"></img>
+        </div>
+        
     )
 }
